fce110087e1803e9dc97d9928957e506b0fb5893
Shani Soni <shanisoni7084@gmail.com>
2024-11-09 17:49:14 +0530

    c

--- Server.js
+++ Server.js
@@ -1,15 +1,15 @@
 const express = require('express');
 const app = express();
 
-app.listen(3000 , () => {
-    console.log("Server Started at port no 3000")
+app.listen(8000 , () => {
+    console.log("Server Started at port no 8000")
 }); 
 
 app.get( '/' , (request , responce ) => {
     responce.send("Hello ji Kaise ho Sare ")
 });
 
-app.post('/api.cars' , ( request , responce ) => {
+app.post('/api/cars' , ( request , responce ) => {
     
     const { name , brand } = request.body;
     console.log(name , brand);
