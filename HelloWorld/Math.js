232ddd1de0457f268994120d7ec0d06137e89372
Shani Soni <shanisoni7084@gmail.com>
2024-11-10 18:47:14 +0530

    C

--- HelloWorld/Math.js
+++ HelloWorld/Math.js
@@ -1,5 +1,12 @@
-const result = require('./Hello')
+// const result = require('./Hello')
 
-console.log(result.add( " The Addition of the a and b is :  ", 81 , 9 ));
+// console.log( " The Addition of the a and b is : ", result.add(81 , 90));
+// console.log( " The Subtraction of the a and b is : ", result.sub(81 , 90));
+// // console.log(result.sub( " The Addition of the a and b is :  ", 81 , 9 ));
 
-console.log(result.sub( " The Addition of the a and b is :  ", 81 , 9 ));
+
+const { add , sub } = require('./Hello')
+
+console.log( " The Addition of the a and b is : ", add(81 , 90));
+console.log( " The Subtraction of the a and b is : ", sub(81 , 90));
+// console.log(result.sub( " The Addition of the a and b is :  ", 81 , 9 ));
