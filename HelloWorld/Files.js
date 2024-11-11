c0fd1acd9786c584b4ed48a2241c9448b3f5e780
Shani Soni <shanisoni7084@gmail.com>
2024-11-11 18:15:09 +0530

    c

--- HelloWorld/Files.js
+++ HelloWorld/Files.js
@@ -1,5 +1,15 @@
+const { Console } = require("console");
 const fs = require("fs");
 
 // fs.writeFileSync("./File.text", "Hello World");
-const result =  fs.readFileSync( "./Contacts.text" , "utf-8");
-console.log(result);
+// const result =  fs.readFileSync( "./Contacts.text" , "utf-8");
+// console.log(result);
+// fs.readFile("./Contacts.text" , "utf-8" , (err , data) => {
+//    if(err) {
+//     console.log(err);
+//    }
+//    else{
+//     console.log(data);
+
+//    }
+// });
