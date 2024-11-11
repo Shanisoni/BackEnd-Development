78098807d07c8c3c14cfd5367b7c459aba9c58f1
Shani Soni <shanisoni7084@gmail.com>
2024-11-11 23:32:12 +0530

    c

--- Server/index.js
+++ Server/index.js
@@ -1,12 +1,19 @@
-const http = require( "http");
+const http = require("http");
+const fs = require('fs');
 
-const myServer = http.createServer( ( request , responce ) => {
+const myServer = http.createServer((request, response) => {
+    const log = `${Date.now()} : ${request.url} New Req Received\n`;
 
-    console.log("New Request Recived .");
-    responce.end("Hello From Server")
+    fs.appendFile("log.txt", log, (error) => {
+        if (error) {
+            console.log("Error writing to file:", error);
+        }
+        response.end("Hello From Server Again");
+    });
 
+    console.log("New Request Received.");
 });
 
-myServer.listen(8000 , () => {
-    console.log("Server Started")
-}); 
+myServer.listen(8000, () => {
+    console.log("Server Started");
+});
