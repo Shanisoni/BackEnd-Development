f98df70e9e9dae313a00caddda4534768997c924
Shani Soni <shanisoni7084@gmail.com>
2024-11-14 17:03:49 +0530

    c

--- Project-1/index.js
+++ Project-1/index.js
@@ -7,16 +7,19 @@ app.get('/api/users' , (req , res) => {
     res.json(users);
 });
 
-app.get('/users' , (req , res) => {
+app.get('/api/users/:id' , (req , res) => {
+    const id  = Number(req.params.id);
+    const user = users.find(user => user.id === id);
+    return res.json(user); 
+})
 
-const html = `
-<ul> 
-${users.map(user => `<li>${user.first_name} ${user.last_name}</li>`).join('')}
-</ul>
-    
+app.get('/users' , (req , res) => {
+ const html = `
+  <ul> 
+   ${users.map(user => `<li>${user.first_name} ${user.last_name}</li>`).join('')}
+  </ul>    
         `;
         res.send(html);
-
-});
+  });
 
 app.listen(PORT , ( ) => console.log('Server is running on port 8000'));
