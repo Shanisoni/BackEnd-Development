d69d9083e2ef4688179abdd1d805e6535636dcc9
Shani Soni <shanisoni7084@gmail.com>
2024-11-14 17:17:29 +0530

    c

--- Project-1/index.js
+++ Project-1/index.js
@@ -7,11 +7,23 @@ app.get('/api/users' , (req , res) => {
     res.json(users);
 });
 
+
 app.get('/api/users/:id' , (req , res) => {
     const id  = Number(req.params.id);
     const user = users.find(user => user.id === id);
     return res.json(user); 
 });
+
+app.post('/api/users' , (req , res) => {
+
+    return res.json( { status : 'Pending'});
+});
+
+app.patch('/api/users' , (req , res) => {
+
+    return res.json( { status : 'Pending'});
+});
+
 
 app.get('/users' , (req , res) => {
  const html = `
