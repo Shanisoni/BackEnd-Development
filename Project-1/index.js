b8d6d8576332d84a171788f09eaf0dbda38604d7
Shani Soni <shanisoni7084@gmail.com>
2024-11-17 16:51:15 +0530

    j

--- Project-1/index.js
+++ Project-1/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const fs = require('fs');
 const app = express();
 const PORT = 8000;
+
 const users = require('./MOCK_DATA.json')
 // Middleware
 app.use(express.urlencoded({ extended: false }));
