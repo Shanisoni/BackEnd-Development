25a5416e67d31138c73dd568503b588046efc7f6
Shani Soni <shanisoni7084@gmail.com>
2024-12-08 17:14:23 +0530

    Commited

--- Server/index.js
+++ Server/index.js
@@ -29,25 +29,42 @@ app.get( '/about' , (request , response) => {
 // if (request.url === "/favicon.ico") {
 //     return response.end();
 // }
+
 // const log = `${Date.now()} :${request.method} ${request.url} New Req Received\n`;
+
 // const myurl = url.parse(request.url, true);
+
 // console.log("Query Params: ", myurl);
 
 // fs.appendFile("log.txt", log, (error ,  data) => {
+
 //     switch (myurl.pathname) {
+
 //         case "/":
+
 //             response.write("Hello From Server");
+
 //             break;
+
 //         case "/about":
+
 //             response.write("This is about page");
+
 //             break;
+
 //         case "/contact":
+
 //             response.write("This is contact page");
+
 //             break;
+
 //         default:
+
 //             response.write("404 Page Not Found");
 //     }
+
 //     if(request.url === "/favicon.ico") {
+
 //         return response.end();
 //     }
 //     if (error) {
