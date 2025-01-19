5c49a151cc362cbd47ae6cea273ca9500b98189a
Shanisoni <shanisoni7084@gmail.com>
2025-01-19 18:36:09 +0530

    c

--- Project-1/connection.js
+++ Project-1/connection.js
@@ -13,3 +13,20 @@ async function connectMongoDb(uri) {
 }
 
 module.exports = { connectMongoDb };
+
+
+// const mongoose = require("mongoose");
+
+// async function connectMongoDb(uri) {
+//     try {
+//         await mongoose.connect(uri, {
+//             useNewUrlParser: true,
+//             useUnifiedTopology: true,
+//         });
+//         console.log("Connected to MongoDB");
+//     } catch (err) {
+//         console.error("MongoDB connection error:", err);
+//     }
+// }
+
+// module.exports = { connectMongoDb };
