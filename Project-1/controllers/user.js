8a515900a039260700ece3b83ec2e7616587b15b
Shani Soni <shanisoni7084@gmail.com>
2025-01-19 18:16:34 +0530

    c

--- Project-1/controllers/user.js
+++ Project-1/controllers/user.js
@@ -1,12 +1,11 @@
 const User = require('../models/user');
 
+
 async function handlwGetAllUser( req , res) {
     const allDbUsers = await User.find({});
     return res.json(allDbUsers);
     
 }
-
-
 
 async function handlegetUserById( req , res , next) {
 
