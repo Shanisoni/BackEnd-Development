f384f16b788da4a998f1eb0b9d2be41501a437a5
Shanisoni <shanisoni7084@gmail.com>
2025-02-03 13:41:35 +0530

    commited

--- Project-1/connection.js
+++ Project-1/connection.js
@@ -20,11 +20,3 @@ module.exports = { connectMongoDb };
 //         await mongoose.connect(uri, {
 //             useNewUrlParser: true,
 //             useUnifiedTopology: true,
-//         });
-//         console.log("Connected to MongoDB");
-//     } catch (err) {
-//         console.error("MongoDB connection error:", err);
-//     }
-// }
-
-// module.exports = { connectMongoDb };
